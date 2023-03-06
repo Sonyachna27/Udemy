e5521ae5586ddd28e2a5f988edc4e05364ddee63
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2023-03-06 14:45:32 +0200

    nav-page

---
+++ navigation/script.js
@@ -0,0 +1,27 @@
+const closed = document.getElementById('close');
+const opening = document.getElementById('open');
+const container = document.querySelector('.container');
+
+opening.addEventListener('click', () => {
+    container.classList.add('show-nav')
+    opening.classList.add('open')
+    closed.style.display = 'block';
+})
+closed.addEventListener('click', () => {
+    container.classList.remove('show-nav')
+    opening.classList.remove('open')
+    closed.style.display = 'none';
+} )
+const image = document.querySelectorAll('.images');
+image.forEach((images) => {
+    images.addEventListener('click', () => {
+       removeActiveClass()
+       images.classList.add('active')
+    })
+    
+})
+function removeActiveClass() {
+    image.forEach(images => {
+        images.classList.remove('active')
+    })
+}
